cffafdcb499ef3c86b9d2cdbf6c5ee5818fe640d
Bernardo-de-Oliveira <124809823+Bernardo-de-Oliveira@users.noreply.github.com>
2023-05-24 14:47:15 -0300

    Update MainVeiculo.java

--- Lista 4/src/ifsc/MainVeiculo.java
+++ Lista 4/src/ifsc/MainVeiculo.java
@@ -1,33 +1,36 @@
 package ifsc;
 
+import java.util.ArrayList;
+
 public class MainVeiculo {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Produto> prod = new ArrayList<>();
+		ArrayList<Veiculo> prod = new ArrayList<>();
 		
 		Veiculo Fusca = new Veiculo();
 		Fusca.setNome("Fusca");
 		Fusca.setAno(2000);
 		Fusca.setCor("Azul");
-		prod.add(1);
+		prod.add(Fusca);
 		
-		Veiculo Fusca = new Veiculo();
-		Fusca.setNome("Corsa");
-		Fusca.setAno(2002);
-		Fusca.setCor("Amarelo");
-		prod.add(2);
+		Veiculo Corsa = new Veiculo();
+		Corsa.setNome("Corsa");
+		Corsa.setAno(2002);
+		Corsa.setCor("Amarelo");
+		prod.add(Corsa);
 		
-		Veiculo Fusca = new Veiculo();
-		Fusca.setNome("Veloster");
-		Fusca.setAno(2012);
-		Fusca.setCor("Preto");
-		prod.add(3);
+		Veiculo Veloster = new Veiculo();
+		Veloster.setNome("Veloster");
+		Veloster.setAno(2012);
+		Veloster.setCor("Preto");
+		prod.add(Veloster);
 		
 		for(int i = 0;i<prod.size();i++) {
 		System.out.println("O carro é um "+ prod.get(i).getNome());
 		System.out.println("do ano "+ prod.get(i).getAno());
 		System.out.println("da cor "+ prod.get(i).getCor());
+		System.out.println("\n");
 		}
 		
 	}

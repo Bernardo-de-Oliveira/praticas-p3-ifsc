27575b2eecd38d567d837fba5d7384a6e2ed76d9
Bernardo-de-Oliveira <124809823+Bernardo-de-Oliveira@users.noreply.github.com>
2023-05-24 15:09:17 -0300

    Lista6

---
+++ Lista6/src/ifsc/Animal.java
@@ -0,0 +1,51 @@
+package ifsc;
+
+public class Animal {
+	
+	private String nome;
+	private String raca;
+	private Float comprimento;
+	private Integer npatas;
+	private String cor;
+	private String eco;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRaca() {
+		return raca;
+	}
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	public Float getComprimento() {
+		return comprimento;
+	}
+	public void setComprimento(Float comprimento) {
+		this.comprimento = comprimento;
+	}
+	public Integer getNpatas() {
+		return npatas;
+	}
+	public void setNpatas(Integer npatas) {
+		this.npatas = npatas;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getEco() {
+		return eco;
+	}
+	public void setEco(String eco) {
+		this.eco = eco;
+	}
+	public void caminha() {
+		
+	}
+
+}
---
+++ Lista6/src/ifsc/MainAnimal.java
@@ -0,0 +1,44 @@
+package ifsc;
+
+public class MainAnimal {
+	public static void main(String[] args) {
+		Cachorro gx = new Cachorro();
+		
+		gx.setNome("Punpun");
+		gx.setRaca("Husky Siberiano");
+		gx.setComprimento((float)0.58);
+		gx.setNpatas(4);
+		gx.setCor("Branco");
+		gx.setEco("Casa");
+		
+		Gato puxar = new Gato();
+		puxar.setNome("Phos");
+		puxar.setRaca("Persa");
+		puxar.setComprimento((float)0.28);
+		puxar.setNpatas(4);
+		puxar.setCor("Laranja");
+		puxar.setEco("Casa");
+		
+		System.out.println(gx.getNome());
+		System.out.println(gx.getRaca());
+		System.out.println(gx.getComprimento());
+		System.out.println(gx.getNpatas());
+		System.out.println(gx.getCor());
+		System.out.println(gx.getEco());
+		System.out.println("");
+		
+		gx.late();
+		
+		System.out.println(puxar.getNome());
+		System.out.println(puxar.getRaca());
+		System.out.println(puxar.getComprimento());
+		System.out.println(puxar.getNpatas());
+		System.out.println(puxar.getCor());
+		System.out.println(puxar.getEco());
+		System.out.println("");
+		
+		puxar.mia();  
+		
+	}
+
+}

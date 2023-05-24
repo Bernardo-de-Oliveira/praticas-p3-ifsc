fdfd010b5a5fedd552f2f8902a871c03c9525272
Bernardo-de-Oliveira <124809823+Bernardo-de-Oliveira@users.noreply.github.com>
2023-05-24 14:39:08 -0300

    Update MainVeiculo.java

--- Lista 4/src/ifsc/MainVeiculo.java
+++ Lista 4/src/ifsc/MainVeiculo.java
@@ -4,14 +4,31 @@ public class MainVeiculo {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Produto> prod = new ArrayList<>();
+		
 		Veiculo Fusca = new Veiculo();
 		Fusca.setNome("Fusca");
 		Fusca.setAno(2000);
 		Fusca.setCor("Azul");
+		prod.add(1);
 		
-		System.out.println("O carro é um "+ Fusca.getNome());
-		System.out.println("do ano "+ Fusca.getAno());
-		System.out.println("da cor "+ Fusca.getCor());
+		Veiculo Fusca = new Veiculo();
+		Fusca.setNome("Corsa");
+		Fusca.setAno(2002);
+		Fusca.setCor("Amarelo");
+		prod.add(2);
+		
+		Veiculo Fusca = new Veiculo();
+		Fusca.setNome("Veloster");
+		Fusca.setAno(2012);
+		Fusca.setCor("Preto");
+		prod.add(3);
+		
+		for(int i = 0;i<prod.size();i++) {
+		System.out.println("O carro é um "+ prod.get(i).getNome());
+		System.out.println("do ano "+ prod.get(i).getAno());
+		System.out.println("da cor "+ prod.get(i).getCor());
+		}
 		
 	}
 	
